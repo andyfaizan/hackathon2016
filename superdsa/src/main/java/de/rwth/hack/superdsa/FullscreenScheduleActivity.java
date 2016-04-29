a806e9e66ae7c7805fae2ab793ca1698a0bee74c
Ainuddin Faizan <andy@scholario.de>
2016-04-29 23:57:46 +0200

    Added map and full screen activity

---
+++ superdsa/src/main/java/de/rwth/hack/superdsa/FullscreenScheduleActivity.java
@@ -0,0 +1,51 @@
+package de.rwth.hack.superdsa;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenScheduleActivity extends AppCompatActivity {
+
+    private View mContentView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen_schedule);
+
+        mContentView = findViewById(R.id.fullscreen_content);
+        if(mContentView != null){
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        hide();
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+}
---
+++ superdsa/src/main/java/de/rwth/hack/superdsa/LocationActivity.java
@@ -0,0 +1,107 @@
+package de.rwth.hack.superdsa;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.dsa.hackathon2016.lib.IVehicleStatus;
+import de.dsa.hackathon2016.lib.VehicleStatusReceiver;
+import de.dsa.hackathon2016.lib.VehicleStatusUtils;
+
+public class LocationActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        String vehicleId = "0410";
+        VehicleStatusUtils.getLastVehicleStatus(vehicleId, new VehicleStatusReceiver() {
+            @Override
+            public void onStatusReceived(IVehicleStatus vehicleStatus) {
+                if (vehicleStatus == null) {
+                    Toast.makeText(LocationActivity.this, "No vehicle status could be obtained.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                float lat = vehicleStatus.getGpsLatitude();
+                float lon = vehicleStatus.getGpsLongitude();
+                LatLng nearestLocation = new LatLng(lat,lon);
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                List<Marker> markersList = new ArrayList<Marker>();
+                final CameraUpdate cu;
+
+                Marker nearestMarker = mMap.addMarker(new MarkerOptions().position(nearestLocation).title("Nearest service centre"));
+                markersList.add(nearestMarker);
+
+                LatLng fakeLocation = new LatLng(lat + 0.01,lon - 0.01);
+                Marker fakeMarker = mMap.addMarker(new MarkerOptions().position(fakeLocation).title("Service centre 2"));
+                markersList.add(fakeMarker);
+
+                LatLng anotherFakeLocation = new LatLng(lat + 0.02,lon + 0.02);
+                Marker anotherFakeMarker= mMap.addMarker(new MarkerOptions().position(anotherFakeLocation).title("Service centre 3"));
+                markersList.add(anotherFakeMarker);
+
+                for (Marker m : markersList) {
+                    builder.include(m.getPosition());
+                }
+
+                int padding = 50;
+
+                LatLngBounds bounds = builder.build();
+                cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+                    @Override
+                    public void onMapLoaded() {
+                        mMap.animateCamera(cu);
+
+                    }
+                });
+
+                mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        Intent intent = new Intent(LocationActivity.this, FullscreenScheduleActivity.class);
+                        startActivity(intent);
+                        // finish();
+                        return false;
+                    }
+                });
+            }
+        });
+    }
+}
